9f8962c95c3066557e2f898c1883f1f939e32b3f
Humberto Lopez Espitia <hlopezes@uci.edu>
2018-05-02 18:53:44 -0700

    Commiting the boulder object I created
    Still figuring out how to spawn it tho

---
+++ Assets/Game Assets/RED SNOW/Scripts/Enemies/BoulderSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		boulder = GameObject.FindGameObjectWithTag ("Boulder");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

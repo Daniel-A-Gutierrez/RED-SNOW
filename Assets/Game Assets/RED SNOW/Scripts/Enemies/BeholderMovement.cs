c8f2bfffe8fcf43a1210bb6759d27f1569a315cb
Anthony Ha <haa4@uci.edu>
2018-05-18 15:04:17 -0700

    Revamped beholder speed and temp hole solution

--- Assets/Game Assets/RED SNOW/Scripts/Enemies/BeholderMovement.cs
+++ Assets/Game Assets/RED SNOW/Scripts/Enemies/BeholderMovement.cs
@@ -7,24 +7,38 @@ public class BeholderMovement : MonoBehaviour {
 	Rigidbody2D rb;
 	public LayerMask layer_mask;
 	float floatHeight = 5f;
-	float horizontalSpeed = 10f;
-
+	float horizontalSpeed = 15f;
+	float horizontalDistanceBuffer = 10f;
+	GameObject player;
+	
 	void Start(){
 		rb = GetComponent<Rigidbody2D>();
+		player = GameObject.FindGameObjectWithTag("Player");
 		rb.velocity = new Vector2(horizontalSpeed,0);
 	}
 
-	// Update is called once per frame
 	void Update () {
 		CheckHeight();
+		CheckHorizontalDistance();
 	}
 
 	void CheckHeight(){
-			RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 20f, layer_mask);
+		RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 30f, layer_mask);
 
-			if (hitInfo.distance != floatHeight){
-				transform.position = new Vector2(transform.position.x, hitInfo.point.y + floatHeight);
-			}
+		if (hitInfo && hitInfo.distance != floatHeight){
+			transform.position = new Vector2(transform.position.x, hitInfo.point.y + floatHeight);
+		}
+	}
 
+	void CheckHorizontalDistance(){
+		float distance_x = transform.position.x - player.transform.position.x;
+		if (distance_x >= 0 && distance_x > horizontalDistanceBuffer){
+			rb.velocity = new Vector2(-horizontalSpeed, 0);
+		}
+		else if (distance_x < 0 && Mathf.Abs(distance_x) > horizontalDistanceBuffer + 10f){
+			rb.velocity = new Vector2(horizontalSpeed * 2f, 0);
+		}
+		else
+			rb.velocity = new Vector2(horizontalSpeed, 0);
 	}
 }
